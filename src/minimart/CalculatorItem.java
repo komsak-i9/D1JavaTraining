361b8c3ab14fd27f1afae011c7567fa718eb2a47
ARIT <ARIT@ARIT-5Y5ZPR1.arit.co.th>
2017-07-18 10:37:23 +0700

    initail

---
+++ src/minimart/CalculatorItem.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minimart;
+
+/**
+ *
+ * @author ARIT
+ */
+public class CalculatorItem extends Item {
+    public static final 
+    
+}
---
+++ src/minimart/ClockItem.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minimart;
+
+/**
+ *
+ * @author ARIT
+ */
+public class ClockItem extends Item{
+    public final String name = "Clock";
+    
+    public ClockItem(int id,double price){
+        super(id,name,price);
+    }
+    
+}
